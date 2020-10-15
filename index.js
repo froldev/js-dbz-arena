d3b0569a7513288bd470a7ec8cb3b292871c9c18
Fred OLIVE <frol.developpement@gmail.com>
2020-10-15 12:20:30 +0200

    create function sleep

--- index.js
+++ index.js
@@ -2,4 +2,10 @@
 function random(max) {
     return Math.floor(Math.random() * Math.floor(max));
     }
+
+// function sleep
+function sleep(delay) {
+    let start = new Date().getTime();
+    while(new Date().getTime() < start + delay);
+    }
     
